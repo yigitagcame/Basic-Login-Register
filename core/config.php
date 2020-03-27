d97f0152241084c8fbe725ed78c7fa35c79229d1
Work <work@Yiitinki-osX-MacBook-Air.local>
2020-03-27 02:22:13 +0200

    public folder integrated

---
+++ core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("URL", "http://localhost"); // Without / at the end
+define("CDIR", "/Users/work/Projects/Php/mageBitHW"); // Without / at the end
+
+define("DB_HOST","");
+define("DB_USER","");
+define("DB_PASS","");
+define("DB_CHARSET","");
+define("DB_ENGINE","");
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'config.php';
-require_once 'core/Bootstrap.php';
-
-$run = new Bootstrap($_GET);
-
-$run->registerController();
-
